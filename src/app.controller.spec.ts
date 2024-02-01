a7a30ba15b111856bd35b657fafe457cb9042101
“tamirmiz631” <tamirmiz631@gmail.com>
2024-02-01 11:24:48 +0200

    tamir

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello mitar!"', () => {
-      expect(appController.getHello()).toBe('yosi!');
+    it('should return "Hello tamir!"', () => {
+      expect(appController.getHello()).toBe('Hello tamir!');
     });
   });
 });
